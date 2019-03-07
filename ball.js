c635350e1c496c0fe497d664537ea71888c686ce
ngoduchai2108 <hai2108hatayqo@gmail.com>
2019-03-08 02:02:42 +0700

    move two Cricle

--- ball.js
+++ ball.js
@@ -3,30 +3,38 @@ const VERY_BOTTOM = 400;
 const MILISECOND = 1;
 const SECONND = 1000;
 const log = console.log;
+let balls = [];
+for (let i=0;i<2;i++){
+    balls[balls.length] = {
+        // x : Math.floor(Math.random()*(VERY_RIGHT-this.r) +this.r),
+        // y : Math.floor(Math.random()*(VERY_BOTTOM-this.r) +this.r),
+        x : Math.floor(Math.random()*VERY_RIGHT),
+        y : Math.floor(Math.random()*VERY_BOTTOM),
+        vx : 1,
+        vy : 1,
+        r : 30
+    };
+}
 
-let ball = {
-    x : 100,
-    y : 100,
-    vx : 1,
-    vy : 1,
-    r : 30
-};
 function drawWar() {
     setTimeout(function () {
         let pen = document.getElementById("myCanvas").getContext('2d')
         pen.clearRect(0,0,VERY_RIGHT,VERY_BOTTOM);
         // drawBall(ball.x,ball.y,ball.r)
-        pen.beginPath();
-        pen.arc(ball.x, ball.y, ball.r, 0, 2*Math.PI)
-        pen.stroke();
-        if (ball.x>VERY_RIGHT - ball.r || ball.x < ball.r){
-            ball.vx = -ball.vx
+        for (let ball of balls){
+            if (ball.x>VERY_RIGHT - ball.r || ball.x < ball.r){
+                ball.vx = -ball.vx
+            }
+            if (ball.y>VERY_BOTTOM - ball.r || ball.y < ball.r){
+                ball.vy = -ball.vy
+            }
+            pen.beginPath();
+            pen.arc(ball.x, ball.y, ball.r, 0, 2*Math.PI)
+            pen.stroke();
+
+            ball.x +=ball.vx;
+            ball.y +=ball.vy;
         }
-        if (ball.y>VERY_BOTTOM - ball.r || ball.y < ball.r){
-            ball.vy = -ball.vy
-        }
-        ball.x +=ball.vx;
-        ball.y +=ball.vy;
         drawWar()
     }, 10*MILISECOND)
 }
