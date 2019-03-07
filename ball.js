1799478badf91a929e0ae55cefceacaba3c42166
ngoduchai2108 <hai2108hatayqo@gmail.com>
2019-03-08 03:19:58 +0700

    fix move two Color Cricle

--- ball.js
+++ ball.js
@@ -1,47 +1,50 @@
-const VERY_RIGHT = 400;
-const VERY_BOTTOM = 400;
+const VERY_RIGHT = 1200;
+const VERY_BOTTOM = 500;
 const MILISECOND = 1;
-const SECONND = 1000;
-const log = console.log;
+// const log = console.log;
 let balls = [];
-// let n = parseInt(prompt(' Enter the ball number'));
-for (let i=0;i<7;i++){
+let n = parseInt(prompt(' Enter the ball number'));
+//document.getElementById('number').value
+for (let i=0;i<n;i++){
     balls[balls.length] = {
         // x : Math.floor(Math.random()*(VERY_RIGHT-this.r) +this.r),
-        // y : Math.floor(Math.random()*(VERY_BOTTOM-this.r) +this.r),
+        // y : Math.floor(Math.random()*(VERY_BOTTOM-this.r) +this.r),//sai
         x : Math.floor(Math.random()*(VERY_RIGHT-100))+50,
         y : Math.floor(Math.random()*(VERY_BOTTOM-100))+50,
-        vx : 1,
-        vy : 1,
+        vx : Math.floor(Math.random()*3)+1,
+        vy : Math.floor(Math.random()*3)+1,
         r : Math.floor(Math.random()*40)+10,
         color : getRandomColor()
     };
 }
 
-function drawWar() {
-    setTimeout(function () {
-        let pen = document.getElementById("myCanvas").getContext('2d')
-        pen.clearRect(0,0,VERY_RIGHT,VERY_BOTTOM);
-        // drawBall(ball.x,ball.y,ball.r)
-        for (let ball of balls){
-            if (ball.x>VERY_RIGHT - ball.r || ball.x < ball.r){
-                ball.vx = -ball.vx
-            }
-            if (ball.y>VERY_BOTTOM - ball.r || ball.y < ball.r){
-                ball.vy = -ball.vy
-            }
-            pen.beginPath();
-            pen.arc(ball.x, ball.y, ball.r, 0, 2*Math.PI)
-            pen.fillStyle = ball.color;
-            pen.fill();
+// function show() {
+    function drawWar() {
+        setTimeout(function () {
+            let pen = document.getElementById("myCanvas").getContext('2d')
+            pen.clearRect(0,0,VERY_RIGHT,VERY_BOTTOM);
+            // drawBall(ball.x,ball.y,ball.r)
+            for (let ball of balls){
+                if (ball.x>VERY_RIGHT - ball.r || ball.x < ball.r){
+                    ball.vx = -ball.vx
+                }
+                if (ball.y>VERY_BOTTOM - ball.r || ball.y < ball.r){
+                    ball.vy = -ball.vy
+                }
+                pen.beginPath();
+                pen.arc(ball.x, ball.y, ball.r, 0, 2*Math.PI)
+                pen.fillStyle = ball.color;
+                pen.fill();
 
-            ball.x +=ball.vx;
-            ball.y +=ball.vy;
-        }
-        drawWar()
-    }, 10*MILISECOND)
-}
-drawWar();
+                ball.x +=ball.vx;
+                ball.y +=ball.vy;
+            }
+            drawWar()
+        }, 10*MILISECOND)
+    }
+    drawWar();
+// }
+// show();
 
 function getRandomHex() {
     return Math.floor(Math.random() * 255)
